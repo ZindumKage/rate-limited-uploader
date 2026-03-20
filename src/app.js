fa724158ae0a01e30b0a7c492aa02040d97c7ccd
octoetix <iam8nd9@gmail.com>
2026-03-20 15:58:28 +0100

    Refactor code structure for improved readability and maintainability

--- src/app.js
+++ src/app.js
@@ -6,17 +6,35 @@ const app = express();
 const loggerMiddleware = require('./middleware/logger.middleware')
 
 app.use(express.json());
+app.use(loggerMiddleware);
 
-app.use ('/api/upload', uploadRoutes);
+app.use('/api/upload', uploadRoutes);
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Rate Limited File Uploader API is running',
+    endpoint: '/api/upload'
+  });
+});
+
 
 app.use((err, req, res, next) => {
+ 
   if (err.name === 'MulterError') {
     return res.status(400).json({
       message: err.message
     });
   }
 
-  next(err);
+ 
+ const logger = require('./utils/logger');
+logger.error(err.message);
+
+  res.status(500).json({
+    message: 'Internal Server Error'
+  });
 });
+
 
 module.exports = app;
